328d039c7868637b6c55f438e497ab7001510b96
Hoang Dang <thanhoangdang@gmail.com>
2020-10-22 11:30:58 +0700

    Add icon for app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import model.Word;
 import service.IWordService;
 import service.WordService;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.List;
+
+
 
 public class Main extends Application {
+    public static Image loadImage(String path) {
+        Image image = null;
+        try {
+            FileInputStream input = new FileInputStream(path);
+            image = new Image(input);
+        } catch (FileNotFoundException e) {
+            System.out.println("Can't load image. Please retry! " + path);
+            e.printStackTrace();
+        }
+        return image;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         URL url = new File("src/main/resources/view/sample.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("DHQ Dictionary");
+        Image dictionary_icon=loadImage("src/main/resources/view/image/dictionary_icon.png");
+        primaryStage.getIcons().add(dictionary_icon);
         primaryStage.setScene(new Scene(root, 968, 747));
         primaryStage.show();
     }
@@ -25,12 +41,12 @@ public class Main extends Application {
     static IWordService wordService = new WordService();
 
     public static void main(String[] args) {
-       // List<Word> list = wordService.findWordsNearMeaning("hell");
+        // List<Word> list = wordService.findWordsNearMeaning("hell");
         //list.forEach(w -> w.printMeaning());
 
 //        Word w = wordService.findExactWord("hello");
 //        w.printMeaning();
-      launch(args);
+        launch(args);
     }
 }
 
